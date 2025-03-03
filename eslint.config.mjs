cd6c4a31efdce17d2b488eb92505dcbec7bde790
mades eladrian <mades.eladrian@gmail.com>
2025-03-03 20:45:04 -0300

    chore: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import love from 'eslint-config-love'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ['**/*.{js,mjs,cjs,ts}']},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ...love,
+    files: ['**/*.js', '**/*.ts'],
+  },
+  {
+    rules: {
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+      'space-before-function-paren': ['error', 'never'],
+      '@typescript-eslint/no-magic-numbers': 'off',
+      'eslint-comments/require-description': 'off',
+      '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+      '@typescript-eslint/prefer-nullish-coalescing': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off'
+    }
+  }
+]
