bf8fb78713c9f87ea807e97f1ff26a8cbc333594
mades eladrian <mades.eladrian@gmail.com>
2025-03-04 11:11:22 -0300

    feat: ensure SignUpController return an error if no name

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,7 +3,8 @@ export class SignUpController {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/class-methods-use-this
   handle(httpRequest: any): any {
     return {
-      statusCode: 400
+      statusCode: 400,
+      body: new Error('Missing param: name')
     }
   }
 }
