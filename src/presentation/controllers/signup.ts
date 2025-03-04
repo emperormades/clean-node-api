07d2af957aa99423826a8ec4af444a1255509245
mades eladrian <mades.eladrian@gmail.com>
2025-03-04 11:02:14 -0300

    feat: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,9 @@
+export class SignUpController {
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/class-methods-use-this
+  handle(httpRequest: any): any {
+    return {
+      statusCode: 400
+    }
+  }
+}
