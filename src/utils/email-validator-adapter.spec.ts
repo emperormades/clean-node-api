ade06e964d101f5defe2384ea8d9237d0718c58f
Mades Almeida <mades.eladrian@gmail.com>
2025-03-08 19:21:03 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -3,7 +3,13 @@ import { EmailValidatorAdapter } from './email-validator-adapter'
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid = sut.isValid('invalid_email@mail.com')
+    const isValid = sut.isValid('invalid_email')
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
   })
 })
