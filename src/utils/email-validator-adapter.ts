c09a70dbff091fe013d838b5e72f44e6853034d2
Mades Almeida <mades.eladrian@gmail.com>
2025-03-08 19:19:58 -0300

    feat: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -1,7 +1,8 @@
 import type { EmailValidator } from '../presentation/contracts'
+import validator from 'validator'
 
 export class EmailValidatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
-    return false
+    return validator.isEmail(email)
   }
 }
