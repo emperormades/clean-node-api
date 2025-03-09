4b5c82584f0c6015864faf2fe34bec0a74dabedc
Mades Almeida <mades.eladrian@gmail.com>
2025-03-09 11:07:34 -0300

    test: ensure BcryptAdapter calls bcrypt with correct values

---
+++ tests/infra/criptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,18 @@
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from '../../../src/infrastructure/criptography/bcrypt-adapter'
+
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => { resolve('hash') })
+  }
+}))
+
+describe('Bcrypt Adapter', () => {
+  test('Should call bcrypt with correct values', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.encrypt('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+})
