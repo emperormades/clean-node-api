8f0ac69c3209a8d6704e36a43ee173163e5baec5
Mades Almeida <mades.eladrian@gmail.com>
2025-03-09 11:10:20 -0300

    test: ensure BcryptAdapter returns a hash on succes

--- tests/infra/criptography/bcrypt-adapter.spec.ts
+++ tests/infra/criptography/bcrypt-adapter.spec.ts
@@ -15,4 +15,11 @@ describe('Bcrypt Adapter', () => {
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
+
+  test('Should return a hash on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash')
+  })
 })
