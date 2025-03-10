d216b2114beeb57739cdd1c6fff112a792d3b8f9
Mades Almeida <mades.eladrian@gmail.com>
2025-03-10 08:57:53 -0300

    feat: connect to mongodb before app listen

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,6 +21,7 @@ export default [
       'promise/avoid-new': 'off',
       'space-before-function-paren': ['error', 'always'],
       'eslint-comments/require-description': 'off',
+      'no-console': 'off',
       '@typescript-eslint/no-magic-numbers': 'off',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'off',
